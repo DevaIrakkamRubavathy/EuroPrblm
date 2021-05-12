47be5c100dea2038c7d028cf13883efe3a9890f7
DevaIrakkamRubavathy <d.i@thoughtworks.com>
2021-05-12 11:00:11 +0530

    [abhishika/Ruba] Add test case for comparing two euro serial numbers

--- src/test/java/com/tw/neew6/EuroTest.java
+++ src/test/java/com/tw/neew6/EuroTest.java
@@ -1,10 +1,10 @@
 package com.tw.neew6;
 
 import com.tw.neev6.Euro;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 
 public class EuroTest {
@@ -13,10 +13,18 @@ public class EuroTest {
         Euro euro1 = new Euro(10);
         Euro euro2 = new Euro(10);
 
-        double euroValue1=euro1.getEuro();
-        double euroValue2=euro2.getEuro();
+        double euroValue1 = euro1.getEuro();
+        double euroValue2 = euro2.getEuro();
 
-        assertEquals(euroValue1,euroValue2);
+        assertEquals(euroValue1, euroValue2);
 
+    }
+
+    @Test
+    void shouldReturnNotEqualWhenSerialNumberOfEurosAreNotEqual() {
+        Euro euro1 = new Euro(10);
+        Euro euro2 = new Euro(10);
+
+        assertNotEquals(euro1, euro2);
     }
 }
