ac5f9972193e1e97464ac32f5509fa358515e0c9
DevaIrakkamRubavathy <d.i@thoughtworks.com>
2021-05-12 10:05:35 +0530

    [abhishika/Ruba] Initial Commit

---
+++ src/test/java/com/tw/neew6/Euro.java
@@ -0,0 +1,10 @@
+package com.tw.neew6;
+
+public class Euro {
+    private double euro1;
+    private double euro2;
+    public Euro(double euro1, double euro2) {
+        this.euro1=euro1;
+        this.euro2=euro
+    }
+}
---
+++ src/test/java/com/tw/neew6/EuroTest.java
@@ -0,0 +1,10 @@
+package com.tw.neew6;
+
+import org.junit.jupiter.api.Test;
+
+public class EuroTest {
+    @Test
+    void shouldReturnEqualWhenValuesOfEurosAreEqual(){
+        Euro euro=new Euro(10, 10);
+    }
+}
