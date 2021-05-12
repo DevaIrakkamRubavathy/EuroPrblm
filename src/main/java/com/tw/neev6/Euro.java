773ba07ce1612da6c30e69b3531a274add21f5fa
DevaIrakkamRubavathy <d.i@thoughtworks.com>
2021-05-12 10:56:06 +0530

    [abhishika/Ruba] Add test case for comparing two euro values

---
+++ src/main/java/com/tw/neev6/Euro.java
@@ -0,0 +1,2 @@
+package com.tw.neev6;public class Euro {
+}
--- src/test/java/com/tw/neew6/EuroTest.java
+++ src/test/java/com/tw/neew6/EuroTest.java
@@ -1,10 +1,22 @@
 package com.tw.neew6;
 
+import com.tw.neev6.Euro;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class EuroTest {
     @Test
-    void shouldReturnEqualWhenValuesOfEurosAreEqual(){
-        Euro euro=new Euro(10, 10);
+    void shouldReturnEqualWhenValuesOfEurosAreEqual() {
+        Euro euro1 = new Euro(10);
+        Euro euro2 = new Euro(10);
+
+        double euroValue1=euro1.getEuro();
+        double euroValue2=euro2.getEuro();
+
+        assertEquals(euroValue1,euroValue2);
+
     }
 }
